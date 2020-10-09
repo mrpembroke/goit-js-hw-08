9555ebedb926fc564ad4edcc4795415dc5bf558c
Dmytro Petrenko <blinker182ca@gmail.com>
2020-10-09 22:43:41 +0300

    done main part of task (gallery)
    without browse arrows

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,58 @@
 import galleryItems from './gallery-items.js';
 
-const gallery = document.querySelector('.js-gallery');
-const lightBox = document.querySelector('.js-lightbox');
-const lightBoxOverlay = document.querySelector('.lightbox__overlay');
-const lightBoxImage = document.querySelector('.ightbox__image');
-const btnClose = document.querySelector('button[data-action="close-lightbox"]');
-
-// console.log(btnClose);
+const refs = {
+  gallery: document.querySelector('.js-gallery'),
+  modal: document.querySelector('.js-lightbox'),
+  lightBoxOverlay: document.querySelector('.lightbox__overlay'),
+  lightBoxImg: document.querySelector('.lightbox__image'),
+  btnClose: document.querySelector('[data-action="close-lightbox"]'),
+};
 
 const imagesList = galleryItems
   .map(element => {
+    const { preview, original, description } = element;
+
     return `<li class="gallery__item">
              <a class="gallery__link" href="">
-                <img class="gallery__image" src="${element.preview}" alt="${element.description}" />
+                <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
              </a>
            </li>`;
   })
   .join('');
 
-gallery.insertAdjacentHTML('afterbegin', imagesList);
+refs.gallery.insertAdjacentHTML('afterbegin', imagesList);
+
+refs.gallery.addEventListener('click', onOpenModal);
+refs.btnClose.addEventListener("click", onCloseModal);
+refs.lightBoxOverlay.addEventListener("click", onCloseModal);
+
+function onOpenModal(e) {
+  // console.log(e);
+  window.addEventListener('keydown', onEscKeyPress);
+  e.preventDefault();
+  
+  if (e.target.nodeName !== "IMG") {
+    return
+  };
+
+  refs.modal.classList.add("is-open");
+  refs.lightBoxImg.src = e.target.dataset.source;
+  refs.lightBoxImg.alt = e.target.alt;
+}
+
+function onCloseModal (e) {
+    // console.log(e);
+  window.removeEventListener('keydown', onEscKeyPress);
+  refs.modal.classList.remove("is-open");
+  refs.lightBoxImg.alt = '';
+  refs.lightBoxImg.src = '';
+}
+
+function onEscKeyPress (e) {
+  // console.log(e);
+  const isEscKey = e.code === "Escape";
+
+  if (isEscKey) {
+    onCloseModal();
+  }
+}
